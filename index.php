eb2a992c01a82326b7dda36571d59860f1d52372
Yahya Mahmud Orhan <e1327280@student.tuwien.ac.at>
2023-06-29 19:31:50 +0200

    added csv-reading and YT-query-loop

--- index.php
+++ index.php
@@ -8,25 +8,51 @@
     require 'credentials.php';
 
     //use YoutubeAPI to check if video is 
-    echo('This Mailer tests if a youtube-Video is online and if it is embeddable'."<br>");
+    echo('This Mailer tests if a youtube-Video is online and if it is embeddable'."\n");
 
     //random testvideo, is gonna be a list later
-    $videoid = "9IQ_ldV9z_A"; 
-    $videoidoffline = "6CQEZ_kas0I";
-    $ytquery = "https://www.googleapis.com/youtube/v3/videos?id=".$videoid."&key=".constant('YTKEY')."&part=snippet,status";
-    echo $ytquery."\n";
-    $response_json=file_get_contents($ytquery);
-    $response=json_decode($response_json);
-    if($response){
-        echo "<br>Response ist angekommen:";
-        if($response->pageInfo->totalResults != 0){
-            echo "<br>".$response->items[0]->snippet->title;
-            echo "<br>".'is embeddable: '.$response->items[0]->status->embeddable;
-        } else {
-            echo "<br>VIDEO IST OFFLINE";
+    $videoid_single = "9IQ_ldV9z_A"; 
+    $videoidoffline_single = "6CQEZ_kas0I";
+
+    $firstline = true;
+
+    $videosToCheck = [];
+
+    $row = 1;
+
+    if (($myfile = fopen("videos.csv", "r")) !== FALSE){
+        while (($data = fgetcsv($myfile, 50, ";")) !== FALSE) {
+            if($row != 1){
+                $num = count(array_filter($data));
+                
+                echo "<p>".($num-1)." videos in course $data[0] <br /></p>\n";
+
+                for ($c=1; $c < $num; $c++) {
+                    array_push($videosToCheck, $data[$c]);
+                    echo $data[$c] . "<br />\n";
+                    
+                }
+            }
+            $row++;
+        }
+        echo "<br><br>Liste der Videos die Online sind:<br>";
+        foreach ($videosToCheck as &$videoID) {
+            echo $videoID;
+
+            $ytquery = "https://www.googleapis.com/youtube/v3/videos?id=".$videoID."&key=".constant('YTKEY')."&part=snippet,status";
+            $response_json=file_get_contents($ytquery);
+            $response=json_decode($response_json);
+            if($response){
+                echo "<br>Response ist angekommen:";
+                if($response->pageInfo->totalResults != 0){
+                    echo "<br>".$response->items[0]->snippet->title;
+                    echo "<br>".'is embeddable: '.$response->items[0]->status->embeddable."<br><br>";
+                } else {
+                    echo "<br>VIDEO IST OFFLINE<br><br>";
+                }
+            }
         }
     }
-
 
     //flag for sending Mails
     $sendMail = false;
